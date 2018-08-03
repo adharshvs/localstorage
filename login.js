29bfcbadda6440b3f71189d92a3fdc16f0aa0d5e
Adharsh S <adharshs@qburst.com>
2018-08-03 14:26:20 +0530

    made code changes

--- login.js
+++ login.js
@@ -1,20 +1,11 @@
-var flag = 0;
-
 function login() {
-    var userName = document.getElementById('uname');
-    var userPass = document.getElementById('pass');
-    users = JSON.parse(localStorage.getItem("users"));    
-    //alert(JSON.stringify(users));
-    for(var user in users) {
-        if(users.hasOwnProperty(user)) {
-            var pass = users[user];
-            if(userName.value == user && userPass.value == pass) {
-                alert("Welcome, " + userName.value + ".");
-                flag = 1;                
-            }        
-        } 
-    }
-    if(flag === 0) {
-        alert("Credentials do not match.");
-    }    
+    var userName = document.getElementById('uname').value;
+    var userPass = document.getElementById('pass').value;
+    if(!localStorage.hasOwnProperty(userName)) {
+        alert("No such account exists");
+    } else if(localStorage.getItem(userName) == userPass) {
+        alert("Welcome, " + userName);
+    } else {
+        alert("Password is wrong.");
+    }     
 }
--- signup.js
+++ signup.js
@@ -1,16 +1,8 @@
-function signup() {    
-    var users = JSON.parse(localStorage.getItem("users")); 
+function signup() {     
     var uname = document.getElementById('uname').value;    
-    var pass = document.getElementById('pass').value;     
+    var pass = document.getElementById('pass').value;    
+    localStorage.setItem(uname, pass);
     
-    users = Object.assign(users, {[uname]: pass});
-    
-    console.log(users);
-    
-    localStorage.setItem("users", JSON.stringify(users));
-    //alert(JSON.parse(localStorage.getItem("users")));
-    //alert(localStorage.getItem("pass"));
     document.getElementById("signup").reset();
-    alert("Account created successfully!");
-    //alert(nm.value + "," + uname.value + "," + pass.value);  
+    alert("Account created successfully!");    
 }
